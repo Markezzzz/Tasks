c712590b189d3bb2414774a7e5f2d54bf8f3d303
Markezzzz <87543713+Markezzzz@users.noreply.github.com>
2021-08-05 13:31:01 +0400

    Update using_array.cs

--- CodeAnalysisTask/using_array.cs
+++ CodeAnalysisTask/using_array.cs
@@ -5,30 +5,30 @@ public static void AddAndSort(ref KeyValuePair<int, string>[] array, KeyValuePai
 }
 
 private static void Sort(ref KeyValuePair<int, string>[] array)
+{
+    var count = 0;
+    while (count != array.Length - 1)
+    {
+        count = 0;
+        for (var j = array.Length - 1; j > 0; j--)
         {
-            var count = 0;
-            while (count != array.Length - 1)
+            if (array[j - 1].Key <= array[j].Key)
             {
-                count = 0;
-                for (var j = array.Length - 1; j > 0; j--)
-                {
-                    if (array[j - 1].Key <= array[j].Key)
-                    {
-                        count++;
-                        continue;
-                    }
-
-                    Swap(ref array, j, j - 1);
-                }
+                count++;
+                continue;
             }
-        }
 
-        private static void Swap(ref KeyValuePair<int, string>[] array, int indexA, int indexB)
-        {
-            var temp = array[indexA];
-            array[indexA] = array[indexB];
-            array[indexB] = temp;
+            Swap(ref array, j, j - 1);
         }
+    }
+}
+
+private static void Swap(ref KeyValuePair<int, string>[] array, int indexA, int indexB)
+{
+    var temp = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = temp;
+}
 
 private static void AddElement(ref KeyValuePair<int, string>[] array, KeyValuePair<int, string> keyValuePair)
 {
